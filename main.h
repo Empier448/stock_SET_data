663897b4161b95af538890a8f1b4d6f2a7b14b06
Add <plaifaeng@hotmail.com>
2024-07-25 14:25:48 +0700

    Add files via upload

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+#include <cstdio>
+#include <chrono>
+#include <thread>
+#include <sstream>
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#endif // MAIN_H
+
